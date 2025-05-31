dcf96c41df75c4b48f22fdc1488fe0f915838742
Hachadi Younes <38957167+shadir69@users.noreply.github.com>
2025-05-31 18:35:58 +0200

    Add files via upload

--- src/pages/Dispute.tsx
+++ src/pages/Dispute.tsx
@@ -183,10 +183,10 @@ const Dispute = () => {
                 <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
                   <h4 className="font-semibold text-yellow-800 mb-2">{t('whatHappensNext')}</h4>
                   <ul className="text-sm text-yellow-700 space-y-1">
-                    <li>• {t('reviewWithin24Hours')}</li>
-                    <li>• {t('emailConfirmation')}</li>
-                    <li>• {t('mayContactForInfo')}</li>
-                    <li>• {t('resolution2to5Days')}</li>
+                      <li>• {t('reviewWithin24Hours')}</li>
+                      <li>• {t('emailConfirmation')}</li>
+                      <li>• {t('mayContactForInfo')}</li>
+                      <li>• {t('resolution2to5Days')}</li>
                   </ul>
                 </div>
 
